264497c8ac3e3670880f8b927be5c9d7a973e8d9
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-07-31 19:19:39 -0700

    Reset

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,3 @@
-use clap::{Parser, Subcommand};
-
-mod host;
-mod cmd;
-mod join;
-
-#[derive(Parser)]
-struct Args {
-    #[clap(subcommand)]
-    commands: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    Host,
-    Join{
-        #[clap(help = "code of the game to join")]
-        code: String,
-    }
-}
-
-fn make_url(endpoint: &String) -> String {
-    "https://rust.nv7haven.com/crufst/".to_string() + endpoint
-}
-
 fn main() {
-    let args = Args::parse();
-    match args.commands {
-        Commands::Host => host::host(),
-        Commands::Join{code} => {join::join(code)}
-    }
+    println!("Hello, World!");
 }
